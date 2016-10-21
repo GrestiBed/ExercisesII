5c488e775f31b7080f3d588b9780c69f0d1bbbd9
GrestiBed <siret@sragentuur.ee>
2016-10-21 11:35:31 +0300

    things

---
+++ src/ASDF/Exercise.java
@@ -0,0 +1,10 @@
+package ASDF;
+
+/**
+ * Created by Siret on 21.10.2016.
+ */
+public class Exercise {
+    public static void exercise(){
+        int hour
+    }
+}
---
+++ src/ASDF/Hello.java
@@ -0,0 +1,28 @@
+package ASDF;
+
+/**
+ * Created by Siret on 19.10.2016.
+ */
+public class Hello {
+    public static void main (String[] args) {
+        System.out.println("Hello, World! Ja Universum.");
+        World.word();
+        Variables.variables();
+        Hours.hours();
+        Arithmetic.arithmetic();
+        Floating.floating();
+        NewLine.newLine();
+        System.out.println("First line.");
+        NewLine.threeLine();
+        System.out.println("Second line.");
+        PrintTwice.printTwice("Super!"); //tuleb kirjutada siia 2 korda see sõna või lause
+
+
+
+
+
+
+
+}
+
+}
